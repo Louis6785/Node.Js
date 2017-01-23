53f840d12baaf14ec1ea44ac916921ae8f291658
Louis6785 <louis_shi@jabil.com>
2017-01-23 13:51:15 +0800

    簡化靜態文件服務器範例代碼

--- static.js
+++ static.js
@@ -10,15 +10,8 @@ var server = http.createServer(function(req, res) {
     var path = join(root, url.pathname);
     // 創建fs.ReadStream
     var stream = fs.createReadStream(path);
-    // 將文件數據寫到响應中
-    stream.on('data', function(chunk) {
-        res.write(chunk);
-    });
-    stream.on('end', function() {
-        // 文件寫完後結束响應
-        res.end();
-    });
-
+    // res.end()會在stream.pipe()內部調用
+    stream.pipe(res);
 });
 
 server.listen(3000);
