6099b83b3cb5eb405dce039bb9a8792a6b0e6a66
Louis6785 <louis_shi@jabil.com>
2017-01-23 14:17:06 +0800

    使用fs.stat()處理錯誤

--- file_server.js
+++ file_server.js
@@ -5,17 +5,34 @@ var fs = require('fs');
 
 var root = __dirname;
 var server = http.createServer(function(req, res) {
+    // 解析URL以獲取路徑名
     var url = parse(req.url);
     // 構造絕對路徑
     var path = join(root, url.pathname);
-    // 創建fs.ReadStream
-    var stream = fs.createReadStream(path);
-    // res.end()會在stream.pipe()內部調用
-    stream.pipe(res);
-    stream.on('error' , function(err) {
-        res.statusCode = 500;
-        res.end('Internal Server Error');
-    });
+    // 檢查文件是否存在
+    fs.stat(path, function(err, stat) {
+        if(err) {
+            if('ENOENT' == err.code) { // 文件不存在
+                res.statusCode = 404;
+                res.end('Not Found');
+            }
+            else { // 其他錯誤
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            }
+        }
+        else
+        {
+            // 用stat對象的屬性設置Content-Length
+            res.setHeader('Content-Length', stat.size);
+            var stream = fs.createReadStream(path);
+            stream.pipe(res);
+            stream.on('error', function(err) {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+        }
+    });    
 });
 
 server.listen(3000);
