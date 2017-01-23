ae7e1903f72bf3604f455b46dcdd2ea9c0a4c0a2
Louis6785 <louis_shi@jabil.com>
2017-01-23 13:33:34 +0800

    添加DELETE功能

--- todo.js
+++ todo.js
@@ -23,6 +23,25 @@ var server = http.createServer(function(req, res) {
             res.setHeader('Content-Type', 'text/plain;charset="utf8"');
             res.end(body);
             break;
+        case 'DELETE':
+            var path = url.parse(req.url).pathname;
+            var i = parseInt(path.slice(1), 10);
+            
+            if(isNaN(i)) { // 檢查數字是否有效
+                res.statusCode = 400;
+                res.end('Invalid item id');
+            }
+            else if(!items[i]) { // 確保請求的索引存在
+                res.statusCode = 404;
+                res.end('Item not found');
+            }
+            else // 刪除請求的事項
+            {
+                items.splice(i, 1);
+                res.end('OK\n');
+            }
+            break;
+
     }
 });
 
