19363605fc133e8dfbe0be0e065aef12c5510422
Louis6785 <louis_shi@jabil.com>
2017-02-10 13:30:11 +0800

    設定出站cookie

--- basic_cookieParser.js
+++ basic_cookieParser.js
@@ -9,3 +9,11 @@ var app = connect()
         res.end('hello\n');
     })
     .listen(3000);
+
+var app2 = connect()
+    .use(function(req, res) {
+        res.setHeader('Set-Cookie', 'foo=bar');
+        res.setHeader('Set-Cookie', 'tobi=ferret;Expires=True, 08 Jun 2021 10:18:14 GMT');
+        res.end();
+    })
+    .listen(3001);
